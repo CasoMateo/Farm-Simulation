0404572ba44e2cc334efc0b1b03acf1eb258e9bb
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-07-15 20:54:10 -0500

    Update scriptgallina.js

--- scriptgallina.js
+++ scriptgallina.js
@@ -1,3 +1,4 @@
+
 const gallina = document.getElementById('crear-gallina');
 const gallina_methods = gallina.querySelectorAll('método-nuevo');
 const crear_gallina = gallina.getElementById('crear-gallina');
@@ -9,6 +10,35 @@ const productos_at = document.getElementById('productos');
 const cantidad_at = document.getElementById('cantidad');
 const hidratado_at = document.getElementById('hidratado');
 const alimentado_at = document.getElementById('alimentado');
+
+localStorage.setItem('gallina-nivel_alimentación', JSON.stringify(0));
+localStorage.setItem('gallina-productos', JSON.stringify(0));
+localStorage.setItem('gallina-cantidad', JSON.stringify(0));
+localStorage.setItem('gallina-hidratado', JSON.stringify(false));
+localStorage.setItem('gallina-alimentado', JSON.stringify(false));
+
+function getDataGallina(e) {
+  var data = new FormData(gallina_form);
+
+  e.preventDefault();
+
+  crear_gallina.style.borderBottom = '2px solid  #dcdee0'
+  
+  for (i = 0; i < gallina_methods.length; i++) {
+    gallina_methods[i].style.display = 'block';
+  }
+  
+  localStorage.setItem('gallina-nombre', JSON.stringify(data.get('crear-gallina-nombre')));
+  localStorage.setItem('gallina-color', JSON.stringify(data.get('crear-gallina-color')));
+  localStorage.setItem('gallina-sonido', JSON.stringify(data.get('crear-gallina-sonido')));
+  localStorage.setItem('gallina-alimento', JSON.stringify(data.get('crear-gallina-alimento')));
+  localStorage.setItem('gallina-precio_alimento', JSON.stringify(data.get('crear-gallina-precio-alimento')));
+  localStorage.setItem('gallina-límite_producto', JSON.stringify(data.get('crear-gallina-límite-producto')));
+  localStorage.setItem('gallina-precio_producto', JSON.stringify(data.get('crear-gallina-precio-producto')));
+  localStorage.setItem('gallina-precio_reja', JSON.stringify(data.get('crear-gallina-precio-reja')));
+  localStorage.setItem('gallina-agua', JSON.stringify(data.get('crear-gallina-agua')));
+
+} 
 
 class Animal {
   // hacer clase abstracta
@@ -145,18 +175,35 @@ export class Gallina extends Animal {
 function hacerSonido(event) {
   event.preventDefault();
 
-  var audio = new Audio('');
-  audio.play();
+  if (localStorage.getItem('gallina-alimentado')) {
+    let new_alimentado = false; 
+    localStorage.setItem('gallina-alimentado', JSON.stringify(new_alimentado));
+
+    var audio = new Audio('');
+    audio.play();
+
+  } else {
+    alert('EL ANIMAL NO ESTÁ ALIMENTADO');
+  }
 }
 
 function mover(event) {
+  
   event.preventDefault();
 
-  const image = document.getElementById('mover-gallina-imagen');
+  if (localStorage.getItem('gallina-alimentado')) {
+    let new_alimentado = false; 
+    localStorage.setItem('gallina-alimentado', JSON.stringify(new_alimentado));
+
+    const image = document.getElementById('mover-gallina-imagen');
   
-  for (i = 0; i < 3; i++) {
-    image.style.marginLeft = '3em';
-    image.style.marginLeft = '15em';
+    for (i = 0; i < 3; i++) {
+      image.style.marginLeft = '3em';
+      image.style.marginLeft = '15em';
+    }
+    
+  } else {
+    alert('EL ANIMAL NO ESTÁ ALIMENTADO')
   }
 
 }
@@ -164,11 +211,20 @@ function mover(event) {
 function saltar(event) {
   event.preventDefault();
 
-  const image = document.getElementById('saltar-gallina-imagen');
+  if (localStorage.getItem('gallina-hidratado')) {
 
-  for (i = 0; i < 3; i++) {
-    image.marginTop = '1em';
-    image.marginTop = '10em';
+    let new_hidratado = false; 
+    localStorage.setItem('gallina-hidratado', JSON.stringify(new_hidratado));
+
+    const image = document.getElementById('saltar-gallina-imagen');
+
+    for (i = 0; i < 3; i++) {
+      image.style.marginTop = '1em';
+      image.style.marginTop = '10em';
+    }
+
+  } else {
+    alert('EL ANIMAL NO ESTÁ HIDRATADO');
   }
 }
 
@@ -176,47 +232,27 @@ function reproducir(event) {
   event.preventDefault();
   var data = new FormData(document.getElementById('reproducir-gallina-form'));
 
-  if (Gallina.cantidad >= 2) {
+  if (localStorage.getItem('gallina-cantidad') >= 2) {
     if (data.get('reproducir-edad1') > 12 && data.get('reproducir-edad2') > 12) {
       if (data.get('reproducir-género1') != data.get('reproducir-género2')) {
-        Gallina.cantidad += data.get('reproducir-cantidad');
-      }
+        let new_cantidad = localStorage.getItem('gallina-cantidad');
+        new_cantidad += data.get('reproducir-cantidad');
+        localStorage.setItem('gallina-cantidad', JSON.stringify(new_cantidad));
     }
-    
   }
     
-  data.get('reproducir-granja-nombre').gastos_ += Gallina.precio_reja_ * data.get('reproducir-cantidad');
-  data.get('reproducir-granja-nombre').dinero_ -= Gallina.precio_reja_ * data.get('reproducir-cantidad');
+  let new_granja_gastos = localStorage.getItem('granja-gastos');
+  new_granja_gastos += localStorage.getItem('granja-precio_reja') * data.get('reproducir-cantidad');
+  localStorage.setItem('granja-gastos', JSON.stringify(new_granja_gastos));
+  let new_granja_dinero = localStorage.getItem('granja-dinero');
+  new_granja_dinero -= localStorage.getItem('granja-precio_reja') * data.get('reproducir-cantidad');
+  localStorage.setItem('granja-dinero', JSON.stringify(new_granja_dinero));
   
 }
 
-function getDataGallina(e) {
-  var data = new FormData(gallina_form);
-
-  e.preventDefault();
-
-  alert(data.get('crear-gallina-nombre'));
-
-  crear_gallina.style.borderBottom = '2px solid  #dcdee0'
-  
-  for (i = 0; i < gallina_methods.length; i++) {
-    gallina_methods[i].style.display = 'block';
-  }
-
-  Gallina.nombre = data.get('crear-gallina-nombre');
-  Gallina.color = data.get('crear-gallina-color');
-  Gallina.sonido = data.get('crear-gallina-sonido');
-  Gallina.alimento = data.get('crear-gallina-alimento');
-  Gallina.precio_alimento = data.get('crear-gallina-precio-alimento');
-  Gallina.límite_producto = data.get('crear-gallina-límite-producto'));
-  Gallina.precio_producto = data.get('crear-gallina-precio-producto');
-  Gallina.precio_reja = data.get('crear-gallina-precio-reja');
-  Gallina.agua = data.get('crear-gallina-agua');
-
-} 
 
 nivel_alimentación_at.addEventListener('mouseover', function() {
-  nivel_alimentación_at.innerText = Gallina.nivel_alimentación;
+  nivel_alimentación_at.innerText = localStorage.getItem('gallina-nivel_alimentación');;
 });
 
 nivel_alimentación_at.addEventListener('mouseout', function() {
@@ -224,7 +260,7 @@ nivel_alimentación_at.addEventListener('mouseout', function() {
 }); 
 
 productos_at.addEventListener('mouseover', function() {
-  productos_at.innerText = Gallina.producto;
+  productos_at.innerText = localStorage.getItem('gallina-productos');
 });
 
 productos_at.addEventListener('mouseout', function() {
@@ -232,7 +268,7 @@ productos_at.addEventListener('mouseout', function() {
 });
 
 cantidad_at.addEventListener('mouseover', function() {
-  cantidad_at.innerText = Gallina.cantidad;
+  cantidad_at.innerText = localStorage.getItem('gallina-cantidad');
 });
 
 cantidad_at.addEventListener('mouseout', function() {
@@ -240,7 +276,7 @@ cantidad_at.addEventListener('mouseout', function() {
 });
 
 hidratado_at.addEventListener('mouseover', function() {
-  hidratado_at.innerText = Gallina.hidratado;
+  hidratado_at.innerText = localStorage.getItem('gallina-hidratado');
 });
 
 hidratado_at.addEventListener('mouseout', function() {
@@ -248,7 +284,7 @@ hidratado_at.addEventListener('mouseout', function() {
 });
 
 alimentado_at.addEventListener('mouseover', function() {
-  alimentado_at.innerText = Gallina.alimentado;
+  alimentado_at.innerText = localStorage.getItem('gallina-alimentado');
 });
 
 alimentado_at.addEventListener('mouseout', function() {
