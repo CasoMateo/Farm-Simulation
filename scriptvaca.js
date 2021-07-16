30abd6600f5d9c62837fe05fe0021fbeddb79cdd
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-07-15 21:04:19 -0500

    Update scriptvaca.js

--- scriptvaca.js
+++ scriptvaca.js
@@ -1,5 +1,3 @@
-import {Granja} from './scriptgranja.js';
-
 const vaca = document.querySelector('.vaca-métodos');
 const vaca_methods = vaca.querySelectorAll('.método-nuevo');
 const crear_vaca = vaca.querySelector('#crear-vaca');
@@ -12,164 +10,88 @@ const cantidad_at = document.getElementById('cantidad');
 const hidratado_at = document.getElementById('hidratado');
 const alimentado_at = document.getElementById('alimentado');
 
-const granja = new Granja();
+localStorage.setItem('vaca-nivel_alimentación', JSON.stringify(0));
+localStorage.setItem('vaca-productos', JSON.stringify(0));
+localStorage.setItem('vaca-cantidad', JSON.stringify(0));
+localStorage.setItem('vaca-hidratado', JSON.stringify(false));
+localStorage.setItem('vaca-alimentado', JSON.stringify(false));
 
-class Animal {
-  // hacer clase abstracta
-	constructor() {
-	  if (new.target === Animal) {
-	    throw new Error('Abstract class cannot be instantiated');
-	  }		
-    this.nivel_alimentación = 0;
-  	this.producto = 0;
-  	this.cantidad = 0;
-  	this.hidratado = false;
-  	this.alimentado = false;
-  }
-  
-  hacer_sonido() {
-    // toggle class alimentado
-    // hacer sonido
-    if (this.alimentado_) {
-      console.log(this.sonido_);
-      this.alimentado_ = false;
-    } else {
-      console.log('No estoy alimentado');
-    }
-  }
-  
-  mover() {
-    // toggle class alimentado
-    // moverse
-    if (this.alimentado_) {
-      console.log(this.nombre_, 'se está moviendo');
-      this.alimentado_ = false;
-    } else {
-      console.log('No estoy alimentado');
-    }
-  }
-  
-  hidratado() {
-    // toggle class hidratado
-    // saltar
-    if (this.hidratado_) {
-      console.log(this.nombre_, 'está saltando porque está hidratado');
-      this.hidratado_ = false;
-    } else {
-      console.log('No estoy hidratado');
-    }
-    
-  }
-  
-  reproducir(edad1, edad2, género1, género2, cantidad, nombre_granja) {
-    if (this.cantidad_ >= 2) {
-      if (edad1 > 12 && edad2 > 12) {
-        if (género1 != género2) {
-          this.cantidad_ += cantidad;
-        }
-      }
-      
-    }
-    
-    nombre_granja.gastos_ += this.precio_reja_ * cantidad;
-    nombre_granja.dinero_ -= this.precio_reja_ * cantidad;
-    
-  }
-  
-  precio() {
-    console.log('Este es el precio del animal');
-  }
+function getDataVaca(event) {
 
-  set nombre(new_nombre) {
-    this.nombre = new_nombre;
-  }
+  var data = new FormData(vaca_form);
 
-  set color(new_color) {
-    this.color = new_color;
-  }
-
-  set sonido(new_sonido) {
-    this.sonido = new_sonido;
-  }
-
-  set alimento(new_alimento) {
-    this.alimento = new_alimento;
-  }
-
-  set precio_alimento(new_precio_alimento) {
-    this.precio_alimento = new_precio_alimento;
-  }
-
-  set límite_producto(new_límite_producto) {
-    this.límite_producto = new_límite_producto;
-  }
-
-  set precio_producto(new_precio_producto) {
-    this.precio_producto = new_precio_producto;
-  }
-
-  set precio_reja(new_precio_reja) {
-    this.precio_reja = new_precio_reja;
-  }
-
-  set agua(new_agua) {
-    this.agua = new_agua;
-  }
-
-}
-
-export class Vaca extends Animal {
-  
-  constructor() {
-    
-      this.nivel_alimentación = 0;
-      this.cantidad = 0;
-      this.producto = 0;
-  	  this.hidratado = false;
-  	  this.alimentado = false;
-    } 
-  
-  
-  precio(productos, tipo, género, peso) {
-    let precio_ = peso;
-    
-    if (género == 'm') {
-      precio_ += 50;
-    }
-    
-    return precio_;
-    
-  }
-  
-}
-
-function hacerSonido(event) {
   event.preventDefault();
 
-  var audio = new Audio('');
-  audio.play();
+  crear_vaca.style.borderBottom = '2px solid  #dcdee0';
+
+  for (i = 0; i < vaca_methods.length; i++) {
+    vaca_methods[i].style.display = 'block';
+  }
+
+  localStorage.setItem('vaca-nombre', JSON.stringify(data.get('crear-vaca-nombre')));
+  localStorage.setItem('vaca-color', JSON.stringify(data.get('crear-vaca-color')));
+  localStorage.setItem('vaca-sonido', JSON.stringify(data.get('crear-vaca-sonido')));
+  localStorage.setItem('vaca-alimento', JSON.stringify(data.get('crear-vaca-alimento')));
+  localStorage.setItem('vaca-precio_alimento', JSON.stringify(data.get('crear-vaca-precio-alimento')));
+  localStorage.setItem('vaca-límite_producto', JSON.stringify(data.get('crear-vaca-límite-producto')));
+  localStorage.setItem('vaca-precio_producto', JSON.stringify(data.get('crear-vaca-precio-producto')));
+  localStorage.setItem('vaca-precio_reja', JSON.stringify(data.get('crear-vaca-precio-reja')));
+  localStorage.setItem('vaca-agua', JSON.stringify(data.get('crear-vaca-agua')));
+
+} 
+
+function hacerSonido(event) {
+
+  event.preventDefault();
+
+  if (localStorage.getItem('vaca-alimentado')) {
+    let new_alimentado = false; 
+    localStorage.setItem('vaca-alimentado', JSON.stringify(new_alimentado));
+
+    var audio = new Audio('');
+    audio.play();
+
+  } else {
+    alert('EL ANIMAL NO ESTÁ ALIMENTADO');
+  }
 }
 
 function mover(event) {
   event.preventDefault();
 
-  const image = document.getElementById('mover-vaca-imagen');
-  
-  for (i = 0; i < 3; i++) {
-    image.style.marginLeft = '3em';
-    image.style.marginLeft = '15em';
-  }
+  if (localStorage.getItem('vaca-alimentado')) {
+    let new_alimentado = false; 
+    localStorage.setItem('vaca-alimentado', JSON.stringify(new_alimentado));
 
+    const image = document.getElementById('mover-vaca-imagen');
+  
+    for (i = 0; i < 3; i++) {
+      image.style.marginLeft = '3em';
+      image.style.marginLeft = '15em';
+    }
+    
+  } else {
+    alert('EL ANIMAL NO ESTÁ ALIMENTADO')
+  }
 }
 
 function saltar(event) {
   event.preventDefault();
 
-  const image = document.getElementById('saltar-vaca-imagen');
+  if (localStorage.getItem('vaca-hidratado')) {
 
-  for (i = 0; i < 3; i++) {
-    image.style.marginTop = '1em';
-    image.style.marginTop = '10em';
+    let new_hidratado = false; 
+    localStorage.setItem('vaca-hidratado', JSON.stringify(new_hidratado));
+
+    const image = document.getElementById('saltar-vaca-imagen');
+
+    for (i = 0; i < 3; i++) {
+      image.style.marginTop = '1em';
+      image.style.marginTop = '10em';
+    }
+
+  } else {
+    alert('EL ANIMAL NO ESTÁ HIDRATADO');
   }
 }
 
@@ -177,47 +99,28 @@ function reproducir(event) {
   event.preventDefault();
   var data = new FormData(document.getElementById('reproducir-vaca-form'));
 
-  if (Vaca.cantidad >= 2) {
+  if (localStorage.getItem('vaca-cantidad') >= 2) {
     if (data.get('reproducir-edad1') > 12 && data.get('reproducir-edad2') > 12) {
       if (data.get('reproducir-género1') != data.get('reproducir-género2')) {
-        Vaca.cantidad += data.get('reproducir-cantidad');
+        let new_cantidad = localStorage.getItem('vaca-cantidad');
+        new_cantidad += data.get('reproducir-cantidad');
+        localStorage.setItem('vaca-cantidad', JSON.stringify(new_cantidad));
       }
     }
     
   }
-    
-  data.get('reproducir-granja-nombre').gastos_ += Vaca.precio_reja_ * data.get('reproducir-cantidad');
-  data.get('reproducir-granja-nombre').dinero_ -= Vaca.precio_reja_ * data.get('reproducir-cantidad');
   
+  let new_granja_gastos = localStorage.getItem('granja-gastos');
+  new_granja_gastos += localStorage.getItem('granja-precio_reja') * data.get('reproducir-cantidad');
+  localStorage.setItem('granja-gastos', JSON.stringify(new_granja_gastos));
+  let new_granja_dinero = localStorage.getItem('granja-dinero');
+  new_granja_dinero -= localStorage.getItem('granja-precio_reja') * data.get('reproducir-cantidad');
+  localStorage.setItem('granja-dinero', JSON.stringify(new_granja_dinero)); 
 }
 
 
-function getDataVaca(e) {
-
-  var data = new FormData(vaca_form);
-
-  e.preventDefault();
-
-  alert(data.get('crear-vaca-nombre'));
-
-  for (i = 0; i < vaca_methods.length; i++) {
-    vaca_methods.style.display = 'block';
-  }
-
-  Vaca.nombre = data.get('crear-vaca-nombre');
-  Vaca.color = data.get('crear-vaca-color');
-  Vaca.sonido = data.get('crear-vaca-sonido');
-  Vaca.alimento = data.get('crear-vaca-alimento');
-  Vaca.precio_alimento = data.get('crear-vaca-precio-alimento');
-  Vaca.límite_producto = data.get('crear-vaca-límite-producto'));
-  Vaca.precio_producto = data.get('crear-vaca-precio-producto');
-  Vaca.precio_reja = data.get('crear-vaca-precio-reja');
-  Vaca.agua = data.get('crear-vaca-agua');
-
-} 
-
 nivel_alimentación_at.addEventListener('mouseover', function() {
-  nivel_alimentación_at.innerText = Vaca.nivel_alimentación;
+  nivel_alimentación_at.innerText = localStorage.getItem('vaca-nivel_alimentación');
 });
 
 nivel_alimentación_at.addEventListener('mouseout', function() {
@@ -225,7 +128,7 @@ nivel_alimentación_at.addEventListener('mouseout', function() {
 }); 
 
 productos_at.addEventListener('mouseover', function() {
-  productos_at.innerText = Vaca.producto;
+  productos_at.innerText = localStorage.getItem('vaca-productos');
 });
 
 productos_at.addEventListener('mouseout', function() {
@@ -233,7 +136,7 @@ productos_at.addEventListener('mouseout', function() {
 });
 
 cantidad_at.addEventListener('mouseover', function() {
-  cantidad_at.innerText = Vaca.cantidad;
+  cantidad_at.innerText = localStorage.getItem('vaca-cantidad');
 });
 
 cantidad_at.addEventListener('mouseout', function() {
@@ -241,7 +144,7 @@ cantidad_at.addEventListener('mouseout', function() {
 });
 
 hidratado_at.addEventListener('mouseover', function() {
-  hidratado_at.innerText = Vaca.hidratado;
+  hidratado_at.innerText = localStorage.getItem('vaca-hidratado');
 });
 
 hidratado_at.addEventListener('mouseout', function() {
@@ -249,14 +152,10 @@ hidratado_at.addEventListener('mouseout', function() {
 });
 
 alimentado_at.addEventListener('mouseover', function() {
-  alimentado_at.innerText = Vaca.alimentado;
+  alimentado_at.innerText = localStorage.getItem('vaca-alimentación');
 });
 
 alimentado_at.addEventListener('mouseout', function() {
   alimentado_at.innerText = 'Alimentado';
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  vaca_submit.addEventListener('click', getDataVaca, false);
- 
-}, false);
